1f86e336b0306a0a2ab3802ade9d4ff12adef5e9
Haz AJ <1560744+hazrulazhar@users.noreply.github.com>
2023-01-15 10:35:50 +0000

    added tina

---
+++ .tina/config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "dfb830a2-d9e1-4eb7-9401-5c70ebf815bb", // Get this from tina.io
+  token: "f704c014a41d77087dc0a86ff080f55f56660d2d", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
